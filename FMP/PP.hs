7c662b868e3e664125569920a8b1ec4a3274c230
Peter Simons <simons@cryp.to>
2010-03-04 12:18:33 +0100

    Re-released all code under GPL version 3.

--- FMP/PP.hs
+++ FMP/PP.hs
@@ -1,3 +1,30 @@
+{- |
+   Module      :  FMP.PP
+   Copyright   :  (c) 2003-2010 Peter Simons
+                  (c) 2002-2003 Ferenc Wágner
+                  (c) 2002-2003 Meik Hellmund
+                  (c) 1998-2002 Ralf Hinze
+                  (c) 1998-2002 Joachim Korittky
+                  (c) 1998-2002 Marco Kuhlmann
+   License     :  GPLv3
+   Maintainer  :  simons@cryp.to
+   Stability   :  provisional
+   Portability :  portable
+ -}
+{-
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program. If not, see <http://www.gnu.org/licenses/>.
+ -}
+
 module FMP.PP (
       Doc,
       empty, text, char, double, int,
--- Setup.lhs
+++ Setup.lhs
@@ -1,5 +1,14 @@
 #!/usr/bin/env runhaskell
 
+> {-
+>    Copyright (c) 2010 Peter Simons <simons@cryp.to>
+>
+>    Copying and distribution of this file, with or without modification, are
+>    permitted in any medium without royalty provided the copyright notice
+>    and this notice are preserved. This file is offered as-is, without any
+>    warranty.
+> -}
+>
 > module Main (main) where
 >
 > import Distribution.Simple
